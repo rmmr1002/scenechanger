445384e8a3e2c9341ccae3793bc9d03aef52f169
Rohan <rmmr1002@gmail.com>
2018-07-03 16:43:13 -0400

    Block commit

---
+++ Assets/Scripts/Menubehavior.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+
+public class Menubehavior : MonoBehaviour {
+     
+     public void scenechanger(string name)
+    {
+        XRSettings.enabled=true;
+        SceneManager.LoadScene(name);
+    }
+
+}
